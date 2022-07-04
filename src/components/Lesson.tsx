0ed9408c6ea82c413cf50062b17863984c3c413e
RodrigoCortezDev <rodrigocortezdev@gmail.com>
2022-07-04 01:25:44 -0300

    Estilizando e realizando mutations

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,7 +1,8 @@
+import classNames from 'classnames';
 import ptBR from 'date-fns/locale/pt-BR';
 import { CheckCircle } from 'phosphor-react';
 import { format, isPast } from 'date-fns';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 interface LessonProps {
 	title: string;
@@ -11,6 +12,9 @@ interface LessonProps {
 }
 
 export default function Lesson(props: LessonProps) {
+	const { slug } = useParams<{ slug: string }>();
+
+	const isActiveLesson = slug === props.slug;
 	const isLessonAvailable = isPast(props.availableAt);
 	const availableAtFormated = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", { locale: ptBR });
 
@@ -18,7 +22,11 @@ export default function Lesson(props: LessonProps) {
 		<Link to={`/event/lesson/${props.slug}`} className="group">
 			<span className="text-gray-300">{availableAtFormated}</span>
 
-			<div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+			<div
+				className={classNames('rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500', {
+					'bg-green-500': isActiveLesson,
+				})}
+			>
 				<header className="flex items-center justify-between">
 					{isLessonAvailable ? (
 						<span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -36,7 +44,11 @@ export default function Lesson(props: LessonProps) {
 						{props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
 					</span>
 				</header>
-				<strong className="text-gray-200 mt-2 block">{props.title}</strong>
+				<strong
+					className={classNames('mt-2 block', { 'text-white': isActiveLesson, 'text-gray-200': !isActiveLesson })}
+				>
+					{props.title}
+				</strong>
 			</div>
 		</Link>
 	);
